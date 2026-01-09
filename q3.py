d1f99f8a8e3c09c661e2f851319d1919bada4860
Chris_Tan <chris.tan55@yahoo.com.sg>
2026-01-09 09:25:44 +0800

    q3.py

--- q3.py
+++ q3.py
@@ -23,9 +23,14 @@ def update_dictionary(dct, key, value):
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
 # - {}, "name", "Alice"
+
 result1 = update_dictionary({}, "name", "Alice")
 print("Task 2 #1:", result1)
+#Task 2 #1: {'name': 'Alice'}
 
 # - {"age": 25}, "age", 26
+
 result2 = update_dictionary({"age": 25}, "age", 26)
 print("Task 2 #2:", result2)
+#Original value for key 'age': 25
+#Task 2 #2: {'age': 26}
