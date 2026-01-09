544e14b8a9621ad073e7bf2c237d101e06a91625
Chris_Tan <chris.tan55@yahoo.com.sg>
2026-01-09 08:43:19 +0800

    q2.py

--- q2.py
+++ q2.py
@@ -5,10 +5,28 @@ def find_and_replace(lst, find_val, replace_val):
     - lst must be a list.
     - Return the modified list.
     """
-    return
+    # Check if lst is a list 
+    if not isinstance(lst, list):
+        return -1
+
+    # Replace all occurrences of find_val with replace_val 
+    for i in range(len(lst)):
+        if lst[i] == find_val:
+            lst[i] = replace_val
+
+    return lst
+
 
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+
+#1: [1, 2, 3, 4, 2, 2], 2, 5
+result1 = find_and_replace([1, 2, 3, 4, 2, 2], 2, 5)
+print("Task 2 #1:", result1)
+
+# Scenario 2: ["apple", "banana", "apple"], "apple", "orange"
+result2 = find_and_replace(["apple", "banana", "apple"], "apple", "orange")
+print("Task 2 #2:", result2)
